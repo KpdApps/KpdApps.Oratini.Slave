0d620034333a0cf68765e42cc21f63362396ecb6
Dmitry [KP0H] Pelevin <dmitry@pelevin.pro>
2017-04-16 15:33:54 +0300

    Init commit

---
+++ Services/OrationiSlaveService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using KpdApps.Orationi.Common.Contracts;
+using KpdApps.Orationi.Common.Interfaces;
+
+namespace KpdApps.Orationi.Slave.Services
+{
+	public class OrationiSlaveService : IOrationiSlaveService
+	{
+		public void Configure(SlaveConfiguration configuration)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
